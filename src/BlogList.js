473355cbe03b10ec827058437e152e6776fad572
konoplyyaa1 <conoplevictorr@gmail.com>
2022-10-19 18:35:48 +0300

    init

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = ({blogs, title, handleDelete}) => {
+
+	return (
+		<div>
+			<h2 className='font-header text-2xl mt-6'>{title}</h2>
+			{blogs.map(blog => (
+				<div className='border border-gray-400 w-full p-4 mt-4' key={blog.id}>
+					<h2 className='font-bold'>{blog.title}</h2>
+					<p className='mb-2'>Written by {blog.author}</p>
+					<button onClick={() => handleDelete(blog.id)} >delete post</button>
+				</div>
+			))}
+		</div>
+	)
+}
+ 
+export default BlogList;
