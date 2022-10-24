d93122092b13c2c55b92a097e51554c200282cc9
konoplyyaa1 <conoplevictorr@gmail.com>
2022-10-24 13:30:02 +0300

    feat(routing) - Add routing functionality

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+	return (
+		<div>
+			<h2>Works!</h2>
+		</div>
+	)
+}
+
+export default Create;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
 	return (
 		<nav className='flex justify-between max-w-5xl mx-auto items-center py-6 border-b border-b-black mb-10'>
 			<h1 className="text-2xl font-header font-bold">React Blog</h1>
 			<div className='flex space-x-4 underline'>
-				<a href='/'>Home</a>
-				<a href='/create'>Create blog</a>
+				<Link to='/'>Home</Link>
+				<Link to='/create'>Create blog</Link>
 			</div>
 		</nav>
 	)
